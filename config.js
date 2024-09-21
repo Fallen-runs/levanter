b2908a1ea29e696f57f4755158af36e5f68d5f54
Fallen-runs <fallendiscord12@gmail.com>
2024-09-21 14:05:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_21bcb0d25bb546471794482093ed7fe2ae').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
